20994d79b3a4ac49cd9fd64fceca22577b454826
Paul Kippes <kippes@gmail.com>
2014-02-08 17:34:27 -0800

    simple slots added

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::turn_on(void)
+{
+  ui->button_on->setDown(true);
+  ui->button_off->setDown(false);
+}
+
+void MainWindow::turn_off(void)
+{
+  ui->button_on->setDown(false);
+  ui->button_off->setDown(true);
+}
