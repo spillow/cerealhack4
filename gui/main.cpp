ad9e3200e172ef3f7229b53f44946cf12d90729f
Scott Pillow <scott.pillow@gmail.com>
2014-02-08 18:30:27 -0800

    First run at getting gui to compile with stk.

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,8 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "WvOut.h"
+#include "Instrmnt.h"
+#include "JCRev.h"
+#include "Voicer.h"
+#include "Skini.h"
+#include "RtAudio.h"
+
+using namespace stk;
+
 int main(int argc, char *argv[])
 {
+    Stk::setSampleRate( 44100.0 );
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
